5f48743df47c3264198145bcf31ef14260cf734d
ThanhNT <thanhnt19146391@gmail.com>
2022-09-14 14:03:10 +0700

    modified:   kinect/testKinect.py

--- kinect/testKinect.py
+++ kinect/testKinect.py
@@ -2,11 +2,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 FOLDER = './depth_frames/'
-
 fileName = FOLDER + '10.npy'
 with open(fileName, 'rb') as f:
     a = np.load(f)
-
 plt.imshow(a)
 plt.colorbar()
 plt.show()
